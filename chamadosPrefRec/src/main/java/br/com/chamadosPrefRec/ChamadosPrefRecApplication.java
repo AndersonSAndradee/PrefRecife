b99c848a7e3afa06c2a3f341c40151ddffbb8d03
AndersonSAndradee <AndersonSandrade2023@gmail.com>
2024-10-24 14:06:34 -0300

    Primeiro commit

---
+++ chamadosPrefRec/src/main/java/br/com/chamadosPrefRec/ChamadosPrefRecApplication.java
@@ -0,0 +1,13 @@
+package br.com.chamadosPrefRec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamadosPrefRecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamadosPrefRecApplication.class, args);
+	}
+
+}
---
+++ chamadosPrefRec/src/test/java/br/com/chamadosPrefRec/ChamadosPrefRecApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.chamadosPrefRec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamadosPrefRecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
